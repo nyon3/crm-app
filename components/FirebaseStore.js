0f7e90b867f2164c4a0aa3b705e50f0b5996280f
nyon3 <tomoya@crosstalk.me>
2021-01-31 18:58:53 +0900

    test update

--- components/FirebaseStore.js
+++ components/FirebaseStore.js
@@ -85,18 +85,24 @@ const FirebaseStore = () => {
     const resetAll = async () => {
         docRef.get().then(querySnapshot => {
             querySnapshot.forEach(doc => {
-                doc.ref.update({
-                    spentTime: 0
-                });
+                if (doc.data().membership == 'lite') {
+                    doc.ref.update({
+                        spentTime: 300
+                    });
+                } else {
+                    doc.ref.update({
+                        spentTime: 720
+                    });
+                }
             });
         }).then(() => console.log('reset all data')).catch((err) => console.log(err))
     };
 
     // Calc Max and spentTime.
     const calc = async () => {
-        let timeLimit = 0
+        // let timeLimit = 0
         const charge = 250
-        const totalTimeForMonth = []
+        // const totalTimeForMonth = []
 
         // 入店時間と退店時間を比較して、トータル時間を計算
         const check = (arrivedHour, arrivedMinute) => {
@@ -107,7 +113,9 @@ const FirebaseStore = () => {
                 // TODO 四捨五入を導入！
                 const dateTo = dayjs().hour(arrivedHour).minute(arrivedMinute);
                 //   Calculate difference between start time and end time.
-                res(dateFrom.diff(dateTo, 'minute'))
+                const difference = dateFrom.diff(dateTo, 'minute');
+                const result = difference * -1
+                res(result)
                 // Set calculated time in Count.
             })
         };
@@ -115,46 +123,44 @@ const FirebaseStore = () => {
         await check(hour, minute).then(res => {
             query.get().then(snapshots => {
                 snapshots.forEach(snapshot => {
-                    console.log(snapshot.id, "=>", snapshot.data());
                     docRef.doc(snapshot.id).update({ spentTime: firebase.firestore.FieldValue.increment(res) })
                 });
             }).then(() => console.log('updated')).catch((err) => console.log(err))
         })
-        // 会員さんの今月使った時間とメンバーシップ情報を取得
-        await query.get().then(snapshots => {
-            snapshots.forEach(snapshot => {
-                totalTimeForMonth.push(snapshot.data().spentTime);
-                if (snapshot.data().membership === 'lite') {
-                    console.log(snapshot.data().membership);
-                    timeLimit = 300
-                } else {
-                    console.log(snapshot.data().membership);
-                    timeLimit = 720
-                }
-            });
-        }).then(() => console.log(timeLimit, totalTimeForMonth[0]));
 
-        // 限界時間から今月使った時間を差し引いて、残り時間を計算する
-        const resolveOverTime = (limit, used) => {
-            return new Promise(resolve => {
-                resolve(limit - used);
-            });
-        }
-        //　超過料金の計算処理を実行
-        // TODO お客様を変更してボタンを押すと合計時間がバグっている
-        await resolveOverTime(timeLimit, totalTimeForMonth[0]).then((res) => {
-            if (res >= 0) {
-                return setFee(`残り時間は ${res}分です`);
-            } else if (res <= -1, res >= -15) {
-                return setFee(`${res}分過ぎています。 料金は${charge}円です。`);
-            } else if (res <= -16, res >= -30) {
-                return setFee(`${res}分過ぎています。 料金は${charge * 2}円です。`);
-            } else if (res <= -31, res >= -45) {
-                return setFee(`${res}分過ぎています。 料金は${charge * 3}円です。`);
-            } else if (res <= -46, res >= -60) {
-                return setFee(`${res}分過ぎています。 料金は${charge * 4}円です。`);
-            } else setFee(`${res}分の超過料金の計算をお願いします。`);
-        })
+        // // 会員さんの今月使った時間とメンバーシップ情報を取得
+        // await query.get().then(snapshots => {
+        //     snapshots.forEach(snapshot => {
+        //         totalTimeForMonth.push(snapshot.data().spentTime);
+        //         if (snapshot.data().membership === 'lite') {
+        //             timeLimit = 300
+        //         } else {
+        //             timeLimit = 720
+        //         }
+        //     });
+        // }).then(() => console.log(timeLimit, totalTimeForMonth[0]));
+
+        // // 限界時間から今月使った時間を差し引いて、残り時間を計算する
+        // const resolveOverTime = (limit, used) => {
+        //     return new Promise(resolve => {
+        //         resolve(limit - used);
+        //     });
+        // }
+        // //　超過料金の計算処理を実行
+        // // TODO お客様を変更してボタンを押すと合計時間がバグっている
+        // await resolveOverTime(timeLimit, totalTimeForMonth[0]).then((res) => {
+        //     if (res >= 0) {
+        //         return setFee(`残り時間は ${res}分です`);
+        //     } else if (res <= -1, res >= -15) {
+        //         return setFee(`${res}分過ぎています。 料金は${charge}円です。`);
+        //     } else if (res <= -16, res >= -30) {
+        //         return setFee(`${res}分過ぎています。 料金は${charge * 2}円です。`);
+        //     } else if (res <= -31, res >= -45) {
+        //         return setFee(`${res}分過ぎています。 料金は${charge * 3}円です。`);
+        //     } else if (res <= -46, res >= -60) {
+        //         return setFee(`${res}分過ぎています。 料金は${charge * 4}円です。`);
+        //     } else setFee(`${res}分の超過料金の計算をお願いします。`);
+        // })
     }
 
 
