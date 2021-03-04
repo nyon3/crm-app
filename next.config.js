28a1ccc7a1fc87d5d61aaa99fed761be8c962fce
nyon3 <tomoya@crosstalk.me>
2021-03-04 23:17:39 +0900

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
     distDir: 'build',
-}
+    pwa: {
+        dest: "public", // swの出力ディレクトリ
+        // runtimeCaching: []
+    },
+});
